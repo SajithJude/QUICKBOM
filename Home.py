ec8f6ca2164e111a9a3230a0dd4123f875fe54ca
Jude Sajith <judesajith.aj@gmail.com>
2023-03-17 19:52:20 -0400

    ssss

--- Home.py
+++ Home.py
@@ -40,8 +40,8 @@ def generate_pdf(sections):
         # Draw the section heading and text on the page
         p1 = fitz.Point(50, 100)
         p2 = fitz.Point(50, 150)
-        page.insert_text(p1, heading, fontname="Arial", fontsize=20, fontcolor=(0, 0, 0))
-        page.insert_text(p2, text, fontname="Arial", fontsize=12, fontcolor=(0, 0, 0))
+        page.insert_text(p1, heading, fontname="Arial", fontsize=20)
+        page.insert_text(p2, text, fontname="Arial", fontsize=12)
 
     # Save the document to a buffer
     buffer = io.BytesIO()
