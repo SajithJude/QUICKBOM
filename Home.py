6bf9a95504c7588d7bc8bdb60cacc9e7d4fe5d50
Jude Sajith <judesajith.aj@gmail.com>
2023-03-17 16:11:39 -0400

    sss

--- Home.py
+++ Home.py
@@ -2,15 +2,17 @@ import streamlit as st
 import io
 import fitz
 
-def generate_pdf():
+def generate_pdf(word):
     # Create a new PDF document
     doc = fitz.open()
 
     # Add a new page to the document
     page = doc.new_page()
+    p = fitz.Point(50, 72)
 
+   
     # Draw some text on the page
-    page.insert_text("Hello, World!")
+    page.insert_text(p, word)
 
     # Save the document to a buffer
     buffer = io.BytesIO()
@@ -27,11 +29,13 @@ def generate_pdf():
 def app():
     # Set the app title
     st.title("PDF Generator")
+    
+    text = st.text_area("Write something")
 
     # Add a button to generate the PDF file
     if st.button("Generate PDF"):
         # Generate the PDF file
-        buffer = generate_pdf()
+        buffer = generate_pdf(text)
 
         # Download the PDF file
         st.download_button(
@@ -41,6 +45,6 @@ def app():
             mime="application/pdf"
         )
 
-        
+
 if __name__ == "__main__":
     app()
